c1cba5bb57cfca10607a6a5dc92d652b48a90ddf
BlackAce <alfredo.gon195@gmail.com>
2019-10-03 23:45:10 -0700

    added mocj data to friends js

---
+++ data/friends.js
@@ -0,0 +1,18 @@
+var friendsArray = [
+    {
+        name: "adrian",
+        photo: "",
+        scores:[
+            5,
+            2,
+            3,
+            3,
+            5,
+            1,
+            2,
+            2,
+            4,
+            5
+        ]
+    }
+]

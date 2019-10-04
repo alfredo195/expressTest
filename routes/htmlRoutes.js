1c23e92ea8d9e2e422818851eaf43840271cb893
BlackAce <alfredo.gon195@gmail.com>
2019-10-03 23:37:23 -0700

    added routes for html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,28 +1,18 @@
-/* app.get("/api/friends/:friends", function(req, res) {
-    var chosen = req.params.friends;
-  
-    console.log(chosen);
-  
-    for (var i = 0; i < friends.length; i++) {
-      if (chosen === friends[i].routeName) {
-        return res.json(friends[i]);
-      }
-    }
-  
-    //return res.send("No character found");
-  
+var path = require("path");
+
+
+module.exports = function(app){
+
+  app.get("/servey", function (req, res){
+    res.sendFile(path.json(__dirname, "../public/servey.html"));
   });
 
-   //display the ppl on the wait list
-   app.get("/api/users/:users", function(req, res) {
-    var chosen = req.params.users;
-  
-    console.log(chosen);
-  
-    for (var i = 0; i < users.length; i++) {
-      if (chosen === users[i].routeName) {
-        return res.json(users[i]);
-      }
-    }
-  
-  }); */
+  app.get("/friends", function (req, res){
+    res.sendFile(path.json(__dirname, "../public/friends.html"));
+  });
+
+  app.get("/home", function (req, res){
+    res.sendFile(path.json(__dirname, "../public/index.html"));
+  });
+
+}

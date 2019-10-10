6ce8bbad01dad64d31b7bd070d5473881681aa00
BlackAce <alfredo.gon195@gmail.com>
2019-10-09 17:41:09 -0700

    fixed the not  looping through the motorcyoe array

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -3,28 +3,58 @@
 var friends = require("../data/friends");
 
 
-module.exports = function(app){
 
-  app.get("/api/friends", function(req, res){
+module.exports = function(app) {
+
+  app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
 
-  app.post("/api/friends", function(req, res){
-    var friend = req.body;
-    var match;
-    var lowestDiff = 100;
-    for(var i in friends){
-      var diff = 0;
-      for(var k in friends[i].score){
-        diff += Math.abs(friend.score[k] - friend[i].score[k])
+ 
+
+  app.post("/api/friends", function(req, res) {
+
+    var bestMatch = {
+      name: "",
+      photo: "",
+      friendDifference: Infinity
+    };
+
+    
+    var userData = req.body;
+    var userScores = userData.scores;
+
+    
+    var totalDifference;
+
+    
+    for (var i = 0; i < friends.length; i++) {
+      var currentFriend = friends[i];
+      totalDifference = 0;
+
+      console.log(currentFriend.name);
+
+      
+      for (var j = 0; j < currentFriend.scores.length; j++) {
+        var currentFriendScore = currentFriend.scores[j];
+        var currentUserScore = userScores[j];
+
+        
+        totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
       }
-      if(diff < lowestDiff){
-        lowestDiff = diff;
-        match = friends[i];
+
+      
+      if (totalDifference <= bestMatch.friendDifference) {
+        
+        bestMatch.name = currentFriend.name;
+        bestMatch.photo = currentFriend.photo;
+        bestMatch.friendDifference = totalDifference;
       }
     }
-    friends.push(friend);
-    res.json(match)
-  })
 
+    
+
+    
+    res.json(bestMatch);
+  });
 };
